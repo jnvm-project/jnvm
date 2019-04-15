fcf449a8569e997e7b07e9f7637722443822892d
Anatole Lefort <anatole.lefort@telecom-sudparis.eu>
2019-04-15 15:06:33 +0200

    jni_map_pmem: use maven-native-plugin to compile native library & load it in JNI Java class

--- src/main/java/eu/telecomsudparis/jnvm/api/PMem.java
+++ src/main/java/eu/telecomsudparis/jnvm/api/PMem.java
@@ -1,6 +1,8 @@
 package eu.telecomsudparis.jnvm.api;
 
 public class PMem {
+    static { System.loadLibrary("jnvm-jni"); }
+
     public native long openPmemRoot(String path, long size);
     public native void freePmemRoot(long addr, long size);
     //TODO closePmemRoot from only path
