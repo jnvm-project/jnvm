95c653aba780dc3c825492f6951dac3a1d67c467
Anatole Lefort <anatole.lefort@telecom-sudparis.eu>
2019-04-15 15:10:34 +0200

    jni_pmem_map: add junit pmem mapping test scenario
        test write to persistent memory and test read from it, in separate
        test method to check whether the value was persisted. (the pmem pool
        is open & close before and after each test method)

---
+++ src/test/java/eu/telecomsudparis/jnvm/PMemTests.java
@@ -0,0 +1,45 @@
+package eu.telecomsudparis.jnvm.api;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
+
+class PMemTests {
+
+    private sun.misc.Unsafe unsafe = net.bramp.unsafe.UnsafeHelper.getUnsafe();
+
+    private final static String PMEM_FILE="/tmp/pMemTests";
+    private final static long POOL_SIZE=1024*1024L;
+    private final static int PERSISTENT_VALUE=1;
+
+    private final PMem pmem = new PMem();
+    private long base;
+
+    @BeforeEach
+    void setup() {
+        base = pmem.openPmemRoot(PMEM_FILE, POOL_SIZE);
+    }
+
+    @Test
+    void writeInt() {
+        Assumptions.assumeTrue(base >= 0);
+
+        unsafe.putInt(base, PERSISTENT_VALUE);
+        Assertions.assertEquals(PERSISTENT_VALUE, unsafe.getInt(base));
+    }
+
+    @Test
+    void readInt() {
+        Assumptions.assumeTrue(base >= 0);
+
+        Assertions.assertEquals(PERSISTENT_VALUE, unsafe.getInt(base));
+    }
+
+    @AfterEach
+    void tearDown() {
+        pmem.freePmemRoot(base, POOL_SIZE);
+    }
+
+}
