f189ad95a78f8467406a7cd8435bcca6cab2232a
Anatole Lefort <anatole.lefort@telecom-sudparis.eu>
2019-07-16 11:27:45 +0200

    persistent_map: transient logic impl for PersistentMap
        No persistency supported right now, only transient logic is
        performed.

--- src/main/java/eu/telecomsudparis/jnvm/api/util/persistent/PersistentHashMap.java
+++ src/main/java/eu/telecomsudparis/jnvm/api/util/persistent/PersistentHashMap.java
@@ -3,84 +3,122 @@ package eu.telecomsudparis.jnvm.api.util.persistent;
 import java.io.Serializable;
 import java.util.Set;
 import java.util.Map;
+import java.util.HashMap;
 import java.util.Collection;
 import java.util.AbstractMap;
 
 public class PersistentHashMap<K,V> extends AbstractMap<K,V>
     implements PersistentMap<K,V> {
 
-    public PersistentHashMap() {
-        //TODO Implement me !
-    }
+    private static final int DEFAULT_SIZE = 16;
 
-    public PersistentHashMap(int initialCapacity) {
-        //TODO Implement me !
+    private transient HashMap<K,Long> index;
+    private transient HashMap<K,V> cache;
+
+    public PersistentHashMap() {
+        this( DEFAULT_SIZE );
     }
 
     public PersistentHashMap(Map<? extends K, ? extends V> m) {
-        //TODO Implement me !
+        this( m.size() );
+        putAll( m );
+    }
+
+    public PersistentHashMap(int initialCapacity) {
+        //TODO Connect to Persistent Pool
+        //TODO Recover Persistent Pool
+        //TODO Get correct new Map sizes (sum)
+
+        if(index == null)
+            index = new HashMap<K,Long>( initialCapacity );
+        if(cache == null)
+            cache = new HashMap<K,V>( initialCapacity );
+
+        //TODO Populate transient Maps
     }
 
     /* Map methods */
 
     public int size() {
-        //TODO Implement me !
-        return 0;
+        return index.size();
     }
 
     public boolean isEmpty() {
-        //TODO Implement me !
-        return false;
+        return index.isEmpty();
     }
 
     public boolean containsKey(Object key) {
-        //TODO Implement me !
-        return false;
+        return index.containsKey( key );
     }
 
     public boolean containsValue(Object value) {
-        //TODO Implement me !
-        return false;
+        return cache.containsValue( value );
     }
 
     public Set<K> keySet() {
-        //TODO Implement me !
-        return null;
+        return index.keySet();
     }
 
     public Collection<V> values() {
-        //TODO Implement me !
-        return null;
+        return cache.values();
     }
 
     public V put(K key, V value) {
-        //TODO Implement me !
-        return null;
+        //TODO Send value to Persistent Pool
+        //TODO Retrieve persistent address
+        //TODO Put address in index Map
+
+        return cache.put( key, value );
     }
 
     public V get(Object key) {
-        //TODO Implement me !
-        return null;
+        V v = cache.get( key );
+        if( v == null ) {
+            Long addr = index.get( key );
+            if( addr == null ) {
+                //TODO Search for value in Persistent Pool
+                //TODO Put addr in index
+            }
+            //TODO Retrieve value from addr in Persistent Pool
+            //TODO Put value in cache
+
+            //For now, best we can do is returning null
+            return null;
+        }
+        return v;
     }
 
     public V remove(Object key) {
-        //TODO Implement me !
-        return null;
+        V v = get( key );
+        cache.remove( key );
+        index.remove( key );
+
+        //TODO Remove value in Persistent Pool
+
+        return v;
     }
 
     public void putAll(Map<? extends K, ? extends V> m) {
-        //TODO Implement me !
+        //TODO Reserve multiple slots in Persistent Pool for atomicity
+        //TODO Put into specific slots
+
+        //For now, just do a regular put
+        for( Map.Entry<? extends K, ? extends V> e : m.entrySet() ) {
+            put( e.getKey(), e.getValue() );
+        }
     }
 
     public void clear() {
-        //TODO Implement me !
+        //TODO Mark Persistent Pool empty
+
+        index.clear();
+        cache.clear();
     }
 
     /* AbstractMap methods */
 
     public Set<Map.Entry<K,V>> entrySet() {
-        //TODO Implement me !
-        return null;
+        return cache.entrySet();
     }
 
     /* ConcurrentMap methods */
