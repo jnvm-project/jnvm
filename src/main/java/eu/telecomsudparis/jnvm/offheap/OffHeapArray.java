0dd51848021ac11c05093186bcb9f87f8e8457c1
Anatole Lefort <anatole.lefort@telecom-sudparis.eu>
2020-01-06 17:04:22 +0100

    WIP: add OffHeapArray as an array of off-heap objects

---
+++ src/main/java/eu/telecomsudparis/jnvm/offheap/OffHeapArray.java
@@ -0,0 +1,138 @@
+package eu.telecomsudparis.jnvm.offheap;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+
+public class OffHeapArray<E extends OffHeapObjectHandle>
+        extends OffHeapObjectHandle implements Iterable<E> {
+
+    /* PMEM Layout :
+     *  | Index | Offset | Bytes | Name   |
+     *  |-------+--------+-------+--------|
+     *  | 0     | 0      | 8     | length |
+     *  end: 8 bytes
+     */
+    private final static long[] offsets = { 0L };
+    private static final long SIZE = 8;
+
+    private static final long indexScale = 8;
+    private static final long baseOffset = 8;
+
+    public long length() { return getLongField( offsets[0] ); }
+    private void setLength(long length) { setLongField( offsets[0], length); }
+
+    private void reset() { setLength( 0L ); }
+    private static final long elemOffset(long index) { return baseOffset + index * indexScale; }
+    private E getElem(long index) { return (E) getHandleField( elemOffset( index ) ); }
+    private void setElem(long index, E e) { setHandleField( elemOffset( index ), e ); }
+    private void unsetElem(long index) { setLongField( elemOffset( index ), -1); }
+
+    //Constructor
+    public OffHeapArray() {
+        super();
+        reset();
+    }
+
+    //Reconstructor
+    public OffHeapArray(long offset) {
+        super( offset );
+    }
+
+    public long size() {
+        return SIZE + length() * indexScale;
+    }
+
+    public boolean contains(Object value) {
+        return false;
+        /*
+        this.anyMatch( v -> v.equals( value ) );
+        */
+    }
+
+    public E get(long index) {
+        return getElem( index );
+    }
+
+    public long add(E e) {
+        long index = length();
+        setElem( index, e );
+        setLength( index + 1 );
+        return index;
+    }
+
+    public E remove(long index) {
+        E oldValue = getElem( index );
+        unsetElem( index );
+        //TODO reshape array
+        return oldValue;
+    }
+
+    public void clear() {
+        reset();
+    }
+
+    public OffHeapArray clone() {
+        return this;
+    }
+
+    //Iterable methods
+    public Iterator<E> iterator() {
+        return new Iterator() {
+            long cursor = 0;
+            long end = length();
+            public boolean hasNext() { return cursor < end; }
+            public E next() {
+                if( hasNext() ) {
+                    return getElem( cursor++ );
+                } else {
+                    throw new NoSuchElementException();
+                }
+            }
+        };
+    }
+
+    /* Set methods */
+    /*
+    @Override
+    public boolean add(E e) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return null;
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+    */
+
+}
