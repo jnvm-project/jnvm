4947f87bb38f3e332ceb3343cc072f494bf783e0
Anatole Lefort <anatole.lefort@telecom-sudparis.eu>
2020-01-06 17:05:07 +0100

    WIP: add RecoverableHashMap - an hybrid map with a volatile mapping backed by a persistent array

---
+++ src/main/java/eu/telecomsudparis/jnvm/util/persistent/RecoverableHashMap.java
@@ -0,0 +1,177 @@
+package eu.telecomsudparis.jnvm.util.persistent;
+
+import java.util.Set;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collection;
+import java.util.AbstractMap;
+
+import eu.telecomsudparis.jnvm.offheap.OffHeapObjectHandle;
+import eu.telecomsudparis.jnvm.offheap.OffHeapArray;
+
+
+public class RecoverableHashMap<K extends OffHeapObjectHandle, V extends OffHeapObjectHandle>
+        extends AbstractMap<K,V> implements PersistentMap<K,V> {
+
+    private static final int DEFAULT_SIZE = 16;
+
+    private transient HashMap<K,Long> index;
+    private OffHeapArray<OffHeapNode<K,V>> table;
+
+    static class OffHeapNode<K extends OffHeapObjectHandle, V extends OffHeapObjectHandle>
+            extends OffHeapObjectHandle implements Map.Entry<K,V> {
+        final static long[] offsets = { 0, 8 };
+        final static long SIZE = 16;
+
+        //Constructor
+        OffHeapNode(K key, V value) {
+            super();
+            setHandleField( offsets[0], key );
+            setHandleField( offsets[1], value );
+        }
+
+        //Reconstructor
+        OffHeapNode(long offset) {
+            super( offset );
+        }
+
+        public long size() { return SIZE; }
+
+        public final K getKey() { return (K) getHandleField( offsets[0] ); }
+        public final V getValue() { return (V) getHandleField( offsets[1] ); }
+        public final V setValue(V newValue) {
+            V oldValue = (V) getHandleField( offsets[1] );
+            setHandleField( offsets[1], newValue );
+            return oldValue;
+        }
+    }
+
+    public RecoverableHashMap(Map<? extends K, ? extends V> m) {
+        this();
+        putAll( m );
+    }
+
+    //Constructor
+    public RecoverableHashMap() {
+        index = new HashMap<>();
+        table = new OffHeapArray<>();
+    }
+
+    //Reconstructor
+    public RecoverableHashMap(long offset) {
+        index = new HashMap<>();
+        table = new OffHeapArray<>( offset );
+
+        for( long i=0; i<table.length(); i++ ) {
+            //OffHeapNode<K,V> entry = table.get( i );
+            //K entryKey = entry.getKey();
+            //V entryValue = entry.getValue();
+            //index.put( entryKey, i );
+            index.put( table.get( i ).getKey(), i );
+        }
+        /*
+        table.forEach( entry -> index.put( entry.getKey(), entry.getValue() ) );
+        */
+    }
+
+    /* Map methods */
+
+    public int size() {
+        return index.size();
+    }
+
+    public boolean isEmpty() {
+        return index.isEmpty();
+    }
+
+    public boolean containsKey(Object key) {
+        return index.containsKey( key );
+    }
+
+    public boolean containsValue(Object value) {
+        return table.contains( value );
+    }
+
+    public Set<K> keySet() {
+        return index.keySet();
+    }
+
+    public Collection<V> values() {
+        //return table.clone();
+        return null;
+    }
+
+    public V put(K key, V value) {
+        Long idx; V oldValue = null;
+        if( (idx = index.get( key )) == null ) {
+            idx = table.add( new OffHeapNode<>( key, value ) );
+            index.put( key, idx );
+        } else {
+            oldValue = table.get( idx ).setValue( value );
+        }
+        return oldValue;
+    }
+
+    public V get(Object key) {
+        Long idx;
+        if( ( idx = index.get( key ) ) != null ) {
+            return table.get( idx ).getValue();
+        }
+        return null;
+    }
+
+    public V remove(Object key) {
+        Long idx; V oldValue = null;
+        if( (idx = index.get( key )) == null ) {
+            oldValue = table.remove( idx ).getValue();
+            index.remove( key );
+        }
+        return oldValue;
+    }
+
+    public void putAll(Map<? extends K, ? extends V> m) {
+        //For now, just do a regular put
+        for( Map.Entry<? extends K, ? extends V> e : m.entrySet() ) {
+            put( e.getKey(), e.getValue() );
+        }
+    }
+
+    public void clear() {
+        table.clear();
+        index.clear();
+    }
+
+    /* AbstractMap methods */
+
+    public Set<Map.Entry<K,V>> entrySet() {
+        //return table.clone();
+        return null;
+    }
+
+    /* ConcurrentMap methods */
+    /*
+     * For now, let's not worry about making this Map thread-safe,
+     * since we should be able to open multiple PersistentMap from the same
+     * Persistent Pool
+     *
+     */
+
+    // Unsafe Mechanics
+    /*
+    private static final sun.misc.Unsafe unsafe;
+    private static final long arrayBaseOffset;
+    private static long keyBaseOffset = 12L;
+    private static long keySize = 256L;
+    private static long valueBaseOffset = 12L;
+    private static long valueSize = 256L;
+    static {
+        try {
+            unsafe = net.bramp.unsafe.UnsafeHelper.getUnsafe();
+            arrayBaseOffset = unsafe.arrayBaseOffset( byte[].class );
+        } catch (Exception e) {
+            throw new Error(e);
+        }
+    }
+    */
+
+}
