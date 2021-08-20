e524ce881fb09b1544c170a4a4105a31a858151a
Anatole Lefort <anatole.lefort@telecom-sudparis.eu>
2021-08-20 18:19:59 +0000

    MemoryAllocator: comments for better readability

--- src/main/java/eu/telecomsudparis/jnvm/offheap/MemoryAllocator.java
+++ src/main/java/eu/telecomsudparis/jnvm/offheap/MemoryAllocator.java
@@ -18,8 +18,12 @@ public class MemoryAllocator implements Iterable<MemoryBlockHandle> {
     private transient Queue<MemoryBlockHandle> reclaimed;
     //private transient Map<Long, MemoryBlockHandle> mappings;
     //TODO Can get rid of mappings, as long as MemoryBlocks are interchangeable
+    //In essence, mappings is only useful if offsets are someday made virtual.
 
     //Constructor
+    //TODO Should be private.
+    //Allocator can only be safely be used after complete recovery, thus
+    //should only be instantiable from static recovery method.
     public MemoryAllocator(long offset, long limit) {
         this.totalMemory = limit - BASE;
         this.offset = offset;
@@ -28,6 +32,28 @@ public class MemoryAllocator implements Iterable<MemoryBlockHandle> {
     }
 
     //Reconstructor
+    /**
+      * Older recovery procedure, where heap could be recovered only from
+      * validity bits.
+      *
+      * Linear heap scan only.
+      * Every invalid block is salvaged.
+      * Every valid block (master) is ressurected s.t. the object may fix
+      *   its state.
+      * Multiblocks (slave) are safely ignored (fixed by master already).
+      *
+      * This procedure assumes no memory leaks may happen during execution,
+      * i.e. object pointers should always be updated atomically and
+      * crash-consistently.
+      * This was achieved with either failure-atomic blocks, or atomic helpers.
+      * The atomic helper was using the « next » bits in a block's header to
+      * store a pointer to the new version of the block, making it possible to
+      * retrieve either the old version or the new one after a crash.
+      *
+      * Deprecated since we decided to allow relaxed pointer updates and that
+      * requires fixing the potential leaks with a costlier recovery procedure.
+      *
+      */
     private static MemoryAllocator recover(long offset, long limit) {
         MemoryAllocator allocator = new MemoryAllocator(offset, limit);
 
@@ -49,6 +75,34 @@ public class MemoryAllocator implements Iterable<MemoryBlockHandle> {
 
         return allocator;
     }
+    /**
+      * More recent recovery procedure, takes in the bitmap of blocks reachable
+      * from the root objects to reconstruct the free list of the allocator.
+      *
+      * Object graph walk as heap scan.
+      * Objects fixes themselves as walking through the graph ressurects them.
+      * Every block not marked in the bitset is unreachable and garbage -
+      *   from a previous run, or earlier object fixup.
+      * Invalid blocks are directely salvaged. Valid ones are freed first.
+      *
+      * This procedure presents no challenge and allows for a more relaxed
+      * programming experience, but currently, generating the BitSet is
+      * sub-optimal. We could go plenty faster.
+      *
+      * TODO Write a graph-walk from the root objects, that works statically
+      * (does not rely on proxies, walk through by directly reading the memory).
+      * Either in one of those two fashions.
+      *  1. Traverse blocks and multiblocks, reading class id in header to get
+      *     which offset in the layout is a pointer and continue.
+      *  2. Traverse blocks and multiblocks, ressurect objects 1 at a time for
+      *     fixup and children addresses, discard proxy and continue.
+      *
+      * Recursively walking the graph using methods from the proxies is slower,
+      * and may instantiate a large number of objects, when really, we don't
+      * have to. Plus, it may interfer with the patterns from the application
+      * for retaining/discarding object proxies.
+      *
+      */
     public static void recover(MemoryAllocator allocator, BitSet marks) {
         //long endIdx = allocator.top() / MemoryBlockHandle.size();
         long endIdx = marks.length();
@@ -109,6 +163,7 @@ public class MemoryAllocator implements Iterable<MemoryBlockHandle> {
             //unsafe.pwb( offset + TOP );
             block = new MemoryBlockHandle( offset + BASE + blockOffset );
         } else {
+            //Always fully clear recycled blocks
             block.clear();
         }
         block.init();
@@ -124,6 +179,8 @@ public class MemoryAllocator implements Iterable<MemoryBlockHandle> {
     public void freeBlock(MemoryBlockHandle block) {
         //block.free();
         block.init();
+        //TODO Decide on clearing validity or whole block header
+        //We don't have to flush, blocks are always cleared on allocation
 
         //unsafe.getAndAddLong( null, offset + SIZE, -block.size() );
 
