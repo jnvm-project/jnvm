7ce450f27163455ca1a77690f8468c2ef20e6fad
Anatole Lefort <anatole.lefort@telecom-sudparis.eu>
2021-09-09 09:38:54 +0000

    docker_ycsb_entrypoint: add EXP_NORUN to skip benchmark runs and only plot graphs

--- docker/ycsb/entrypoint.sh
+++ docker/ycsb/entrypoint.sh
@@ -17,11 +17,19 @@ require_env_var "EXP_OUTDIR"
 hard_jdkpath="/home/anatole/jdk8u/build/linux-x86_64-normal-server-release/jdk"
 mkdir -p $hard_jdkpath && mount --bind $JAVA_HOME $hard_jdkpath
 
-#hacky way to clear either one are all old exp records in one container start
-rm -rf ${EXP_OUTDIR}/${EXP_NAME//_/.}*.ref/*
-
 cd /ycsb/exp/
-./${EXP_NAME:-runall}.sh
+
+case $EXP_NORUN in
+  y|yes|true)
+    echo "EXP_NORUN set to $EXP_NORUN, skipping ${EXP_NAME:-every} run"
+    ;;
+  *)
+    #hacky way to clear either one are all old exp records in one container start
+    rm -rf ${EXP_OUTDIR}/${EXP_NAME//_/.}*.ref/*
+    ./${EXP_NAME:-runall}.sh
+    ;;
+esac
+
 ./log_to_data.sh ${EXP_NAME//_/.}
 
 for exp_plot in /ycsb/plot/${EXP_NAME}*.gp ; do
