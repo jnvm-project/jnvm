ffd7d2f0ec3a802a043382ad433118e4213d4439
Anatole Lefort <anatole.lefort@telecom-sudparis.eu>
2021-09-09 20:47:55 +0000

    docker_tpcb_entrypoint: add EXP_NORUN to skip benchmark runs and only plot graphs

--- docker/tpcb_client/entrypoint.sh
+++ docker/tpcb_client/entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-docker pull gingerbreadz/transactions:latest
-
-rm -f /${EXP_OUTDIR}/*
-
-/tpcb/src/test/bin/fault.sh
+case $EXP_NORUN in
+  y|yes|true)
+    echo "EXP_NORUN set to $EXP_NORUN, skipping $EXP_NAME run"
+    ;;
+  *)
+    docker pull gingerbreadz/transactions:latest
+    rm -rf ${EXP_OUTDIR}/*
+    /tpcb/src/test/bin/fault.sh
+    ;;
+esac
 
 gnuplot -e "outdir='${EXP_OUTDIR}'" /tpcb/plot.gp
