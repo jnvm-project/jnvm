97ec4b63389992ea4a043c52b0fe404ea08a4a15
Anatole Lefort <anatole.lefort@telecom-sudparis.eu>
2021-11-10 13:48:56 +0100

    jnvm-transformer: plugin matches annotated types with Persistent.class

--- jnvm-transformer/src/main/java/eu/telecomsudparis/jnvm/transformer/JNVMTransformerPlugin.java
+++ jnvm-transformer/src/main/java/eu/telecomsudparis/jnvm/transformer/JNVMTransformerPlugin.java
@@ -5,12 +5,13 @@ import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.dynamic.ClassFileLocator;
 import net.bytebuddy.dynamic.DynamicType;
 
-import static net.bytebuddy.matcher.ElementMatchers.named;
+import eu.telecomsudparis.jnvm.transformer.annotations.Persistent;
 
 public class JNVMTransformerPlugin implements Plugin {
 
+    //TODO allow un-annotated types from a given list
     public boolean matches(TypeDescription target) {
-        return true;
+        return target.getDeclaredAnnotations().isAnnotationPresent(Persistent.class);
     }
 
     public DynamicType.Builder<?> apply(DynamicType.Builder<?> builder,
