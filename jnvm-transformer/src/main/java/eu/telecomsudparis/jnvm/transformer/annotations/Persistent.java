2f608463cb96294c76ad5714a0ff6a05a4fe6b1f
Anatole Lefort <anatole.lefort@telecom-sudparis.eu>
2021-11-10 13:44:46 +0100

    jnvm-transformer: Persistent annotation ElementType & RetentionPolicy

--- jnvm-transformer/src/main/java/eu/telecomsudparis/jnvm/transformer/annotations/Persistent.java
+++ jnvm-transformer/src/main/java/eu/telecomsudparis/jnvm/transformer/annotations/Persistent.java
@@ -1,5 +1,12 @@
 package eu.telecomsudparis.jnvm.transformer.annotations;
 
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Persistent {
     String fa() default "";
 }
