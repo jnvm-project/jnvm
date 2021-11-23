b9fa747cb027f7ea06f1f960cc958f3ff43828de
Anatole Lefort <anatole.lefort@telecom-sudparis.eu>
2021-11-23 14:04:08 +0100

    jnvm-transformer: fix & re-enable inheritance unit tests
        Use custom in-memory ClassFileLocator and custom TypePool to inject
        instrumented types to and resolve parent types from.

--- jnvm-transformer/src/test/java/eu/telecomsudparis/jnvm/transformer/JNVMTransformerPluginTest.java
+++ jnvm-transformer/src/test/java/eu/telecomsudparis/jnvm/transformer/JNVMTransformerPluginTest.java
@@ -1,75 +1,89 @@
 package eu.telecomsudparis.jnvm.transformer;
 
-import java.util.Collection;
-import java.util.Arrays;
-import java.util.stream.Stream;
 import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.build.Plugin;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.dynamic.ClassFileLocator;
-import net.bytebuddy.dynamic.TypeResolutionStrategy;
+import net.bytebuddy.dynamic.DynamicType;
 import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
+import net.bytebuddy.pool.TypePool;
 
 import eu.telecomsudparis.jnvm.offheap.OffHeapObject;
-import eu.telecomsudparis.jnvm.transformer.sample.*;
 
 class JNVMTransformerPluginTest {
 
-    static Stream<Class<?>> types() {
+    private static final String SAMPLE_PACKAGE = "eu.telecomsudparis.jnvm.transformer.sample.";
+    private static Stream<String> types() {
         return Stream.of(
-            FourLongs.class,
-            Simple.class,
-            //Complexe.class,
-            InvalidSimple.class
-        );
+                "FourLongs",
+                "Simple",
+                "Complexe",
+                "InvalidSimple")
+            .map(SAMPLE_PACKAGE::concat);
     }
 
+    private static Map<String, byte[]> instrumentedTypes;
+
     private Plugin plugin;
+    private ClassFileLocator classFileLocator;
+    private TypePool typePool;
 
     @BeforeAll
     static void setupAll() {
-        //Clear
+        instrumentedTypes = new HashMap<>();
     }
 
     @BeforeEach
     void setup() {
         plugin = new JNVMTransformerPlugin();
+        classFileLocator = new ClassFileLocator.Compound(
+                new ClassFileLocator.Simple(instrumentedTypes),
+                ClassFileLocator.ForClassLoader.of(getClass().getClassLoader()));
+        typePool = new TypePool.Default(
+                new TypePool.CacheProvider.Simple(), classFileLocator,
+                TypePool.Default.ReaderMode.FAST);
     }
 
     @ParameterizedTest
     @MethodSource("types")
-    public void testMatches(Class<?> type) throws Exception {
-        Assertions.assertTrue(plugin.matches(TypeDescription.ForLoadedType.of(type)));
+    public void testMatches(String typeName) throws Exception {
+        TypeDescription type = TypePool.Default.ofSystemLoader().describe(typeName).resolve();
+        Assertions.assertTrue(plugin.matches(type));
     }
 
     @ParameterizedTest
     @MethodSource("types")
-    public void testApply(Class<?> type) throws Exception {
-        Class<?> transformed =
+    public void testApply(String typeName) throws Exception {
+        TypeDescription type = typePool.describe(typeName).resolve();
+
+        DynamicType.Unloaded<?> dynamicType =
             plugin.apply(
-                new ByteBuddy().redefine(type),
-                TypeDescription.ForLoadedType.of(type),
-                ClassFileLocator.ForClassLoader.of(type.getClassLoader()))
-            .make()
+                new ByteBuddy().redefine(type, classFileLocator), type, classFileLocator)
+            .make();
+
+        byte[] typeBytes = classFileLocator.locate(typeName).resolve();
+        byte[] dynamicTypeBytes = dynamicType.getBytes();
+        Assertions.assertNotNull(typeBytes);
+        Assertions.assertNotNull(dynamicTypeBytes);
+        Assertions.assertNotSame(dynamicTypeBytes, typeBytes);
+        Assertions.assertNotEquals(dynamicTypeBytes, typeBytes);
+
+        instrumentedTypes.put(type.getName(), dynamicTypeBytes);
+        Class<?> transformed = dynamicType
             .load(getClass().getClassLoader(), ClassLoadingStrategy.Default.CHILD_FIRST)
             .getLoaded();
 
         Assertions.assertNotNull(transformed);
-        Assertions.assertNotSame(transformed, type);
-        Assertions.assertNotEquals(transformed, type);
-
         Assertions.assertTrue(isPersistent(transformed), "implements OffHeapObject");
         Assertions.assertTrue(hasField(transformed, "CLASS_ID"), "has CLASS_ID");
         Assertions.assertTrue(hasField(transformed, "SIZE"), "has SIZE");
@@ -106,12 +120,14 @@ class JNVMTransformerPluginTest {
 
     @AfterEach
     void tearDown() {
-        //Clear
+        plugin = null;
+        classFileLocator = null;
+        typePool = null;
     }
 
     @AfterAll
     static void tearDownAll() {
-        //Clear
+        instrumentedTypes = null;
     }
 
 }
