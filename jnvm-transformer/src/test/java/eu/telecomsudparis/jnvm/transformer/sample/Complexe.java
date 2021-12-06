b8431e8e2532fa833e5eae35adcfc27589332393
Anatole Lefort <anatole.lefort@telecom-sudparis.eu>
2021-12-06 10:13:44 +0100

    jnvm-transformer: remove temporarily unsupported field types in Complex sample class

--- jnvm-transformer/src/test/java/eu/telecomsudparis/jnvm/transformer/sample/Complexe.java
+++ jnvm-transformer/src/test/java/eu/telecomsudparis/jnvm/transformer/sample/Complexe.java
@@ -9,57 +9,57 @@ import eu.telecomsudparis.jnvm.offheap.OffHeapString;
 public class Complexe extends Simple {
 
     private byte by;
-    private short s;
+    //private short s;
     private int i;
     private long l;
-    private float f;
+    //private float f;
     private double d;
     private char c;
     private OffHeapString str;
-    private boolean bo;
+    //private boolean bo;
 
     private transient byte tby;
-    private transient short ts;
+    //private transient short ts;
     private transient int ti;
     private transient long tl;
-    private transient float tf;
+    //private transient float tf;
     private transient double td;
     private transient char tc;
     private transient String tstr;
-    private transient boolean tbo;
+    //private transient boolean tbo;
 
     public Complexe(byte by, short s, int i, long l, float f, double d, char c, String str, boolean bo) {
         super(0);
         this.by = by;
-        this.s = s;
+        //this.s = s;
         this.i = i;
         this.l = l;
-        this.f = f;
+        //this.f = f;
         this.d = d;
         this.c = c;
         this.str = new OffHeapString(str);
-        this.bo = bo;
+        //this.bo = bo;
     }
 
     private void cache() {
         this.tby = by;
-        this.ts = s;
+        //this.ts = s;
         this.ti = i;
         this.tl = l;
-        this.tf = f;
+        //this.tf = f;
         this.td = d;
         this.tc = c;
         this.tstr = str.toString();
-        this.tbo = bo;
+        //this.tbo = bo;
     }
 
     public void inc() {
         cache();
         this.by++;
-        this.s++;
+        //this.s++;
         this.i++;
         this.l++;
-        this.f++;
+        //this.f++;
         this.d++;
         this.c++;
         /*
